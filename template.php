eed7193393ba3be7d7b9cc8d2b44cd00748a64cd
kevallakhani95 <kevallakhani95@yahoo.com>
2017-05-01 23:46:49 -0400

    Added template dummy file :file_folder:

---
+++ template.php
@@ -0,0 +1,33 @@
+<?php
+ini_set('mysql.connect_timeout', 300);
+ini_set('default_socket_timeout', 300);
+error_reporting(0);
+?>
+
+<html>
+<head>
+
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>LaunchPad</title>
+
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha384-+ENW/yibaokMnme+vBLnHMphUYxHs34h9lpdbSLuAwGkOKFRl4C34WkjazBtb7eT" crossorigin="anonymous">
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php
+session_start();										
+require 'db_conn.php';	
+require 'navbar.php';								
+$user_name = $_SESSION['user_session'];
+
+
+
+?>
+
+
+</body>
+</html>
